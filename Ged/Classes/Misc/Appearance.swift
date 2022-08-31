46e6e5c3aa78615410dba45d466c4e0876e8d84c
akonst17 <akonst17@gmail.com>
2022-08-31 17:57:44 +0300

    вординг; build 16

--- Ged/Classes/Misc/Appearance.swift
+++ Ged/Classes/Misc/Appearance.swift
@@ -15,7 +15,7 @@ struct Appearance {
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
-    static let backgroundColor = UIColor(integralRed: 252, green: 249, blue: 249)
+    static let backgroundColor = UIColor(integralRed: 249, green: 245, blue: 240)
     static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
     static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
