69d651c1347bf24200595ca57b65dfb635fcf26f
akonst17 <akonst17@gmail.com>
2022-09-19 21:15:15 +0300

    интеграция sdk 2.0; новые урлы; build 19

--- Ged/Classes/Misc/GlobalDefinitions.swift
+++ Ged/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams.mastrapi.com"
     static let apiKey = "p1Wu8hWUvE8F0AgO"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "bcEqKBVT6Tb8q219"
-
     static let applicationTag = "EXAM_GED"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "bcEqKBVT6Tb8q219"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "npPchdm9BoGHQMnb"
 
     static let appStoreUrl = "https://apps.apple.com/app/ged-study/id1564276142"
     static let appleAppID = "1564276142"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
